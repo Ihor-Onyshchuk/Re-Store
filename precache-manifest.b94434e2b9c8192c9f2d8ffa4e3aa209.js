b84f0cbc2d1f36d2e0ed72d1d16b3c60d5953650
Ihor-Onyshchuk <onyshchukihor11@gmail.com>
2020-03-30 14:56:36 +0300

    Updates

---
+++ precache-manifest.b94434e2b9c8192c9f2d8ffa4e3aa209.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a6b69d3b37e1809c929fe68b837fa87",
+    "url": "/Re-Store/index.html"
+  },
+  {
+    "revision": "e44154160999de6b32a8",
+    "url": "/Re-Store/static/css/main.26b6fc31.chunk.css"
+  },
+  {
+    "revision": "c7b6599bffb2e21492a7",
+    "url": "/Re-Store/static/js/2.6adb56bb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Re-Store/static/js/2.6adb56bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e44154160999de6b32a8",
+    "url": "/Re-Store/static/js/main.65167da9.chunk.js"
+  },
+  {
+    "revision": "a6544b0cf9399a3fce22",
+    "url": "/Re-Store/static/js/runtime-main.893579e3.js"
+  }
+]);
